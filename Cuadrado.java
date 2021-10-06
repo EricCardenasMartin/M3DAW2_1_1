db90d0cb37fbc86db9093b7abcc183006ae10608
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2021-10-06 18:17:46 +0000

    a

--- Cuadrado.java
+++
@@ -1,7 +0,0 @@
-// public class Cuadrado {
-// 	float lado;
-
-// 	// public Caudrado(){
-		
-// 	// }
-// }
--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    // System.out.println("Hello world!");
-	
-	// Animal paco = new Animal(4, "Paco");
-
-	// paco.Algo();
-
 	PosicioPuntXY paco = new PosicioPuntXY();
 
 	paco.algo();
