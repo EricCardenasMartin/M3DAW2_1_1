78f3e60c093c3e8abf57c93fcb4b1b41633ef4c5
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2021-10-22 15:49:26 +0000

    Acabado v1.1 22/10/2021

---
+++ TestCalculadora.java
@@ -0,0 +1,16 @@
+public class TestCalculadora{
+	public TestCalculadora(int n){
+		System.out.println("Factorial de " + n + ": " + Calculadora.Factorial(n));
+
+		System.out.println("");
+		System.out.println("-------------------------");
+		System.out.println("");
+		System.out.println("Pares menores a " + n + ":");
+		Calculadora.LesserEven(n);
+
+		System.out.println("");
+		System.out.println("-------------------------");
+		System.out.println("");
+		System.out.println(n + (Calculadora.IsPrime(n)?"":" no ") + " es un numero primo");
+	}
+}
