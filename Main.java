56a56c81785500a31f1ebc08045c688ce6382038
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2021-11-15 19:28:52 +0000

    acabado v1.0 15/11/2021

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 class Main {
   public static void main(String[] args) {
-	  String input = "7531321231357";
+	  int[] a = new int[10];
 
-	  boolean isPalindrome = PalindromeManager.IsPalindrome(input);
+	  for(int i = 0; i < a.length; i++){
+		  a[i] = ThreadLocalRandom.current().nextInt(0, 65000 + 1);
+	  }
+  
+		a = Sort.Bubble(a);
 
-	System.out.println(input + (isPalindrome ? " si " : " no ") + "es un palindromo " + (isPalindrome ? ":D" : "D:"));
+		int b = a[a.length-1];
+
+		List<String> aHex = Parse.ToHexadecimal(a[a.length-1]);
+
+		int[] reversedA = Sort.Reverse(a);
   }
 }
